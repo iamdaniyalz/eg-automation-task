9713c80e422601115ecf243b72d47b6b74daf616
Daniyal <iamdaniyalz.pro@gmail.com>
2023-04-01 22:53:58 +0500

    added and applied prettier

--- App/serveApp.js
+++ App/serveApp.js
@@ -1,12 +1,12 @@
-import { createServer } from 'http';
-import { readFileSync } from 'fs';
+import { createServer } from "http";
+import { readFileSync } from "fs";
 
-const fileContent = readFileSync('task.html')
-const server = createServer((req, res)=>{
-    res.writeHead(200, {'Content-type' : 'text/html'}); 
-    res.end(fileContent)
-})
+const fileContent = readFileSync("task.html");
+const server = createServer((req, res) => {
+	res.writeHead(200, { "Content-type": "text/html" });
+	res.end(fileContent);
+});
 
-server.listen(1025, '127.0.0.1', () => {
-    console.log("Application running on http://127.0.0.1:1025");
+server.listen(1025, "127.0.0.1", () => {
+	console.log("Application running on http://127.0.0.1:1025");
 });
